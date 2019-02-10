ff482d5bcfa0080efa4de881a5264f7fe93bbe23
Ryan Shackleton <ryshack@uw.edu>
2019-02-10 16:55:30 -0500

    Move start script to bin/www

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ app.get('/wikipedia-school-shootings', parseRequestFromTemplate(schoolShootings)
 
 app.get('/wikipedia-mass-shootings-pre-2018', parseRequestFromTemplate(massShootingsPre2018));
 
-app.listen('8081');
-console.log('Magic happens on port 8081');
 exports = module.exports = app;
