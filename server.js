fb02d0a2ced68ae5eb93e7eee73718a369ad3bce
Ryan Shackleton <ryshack@uw.edu>
2019-02-10 19:38:48 -0500

    Try allowing cors.

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app     = express();
 const { parseRequestFromTemplate } = require('./utils/parser');
 const { schoolShootings, massShootingsPre2018 } = require('./templates/wikipedia');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/wikipedia-school-shootings', parseRequestFromTemplate(schoolShootings));
 
 app.get('/wikipedia-mass-shootings-pre-2018', parseRequestFromTemplate(massShootingsPre2018));
